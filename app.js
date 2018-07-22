1259ec7399dcf98114b67dc9f916e7fb9b2f6857
Jordan Bourne <jordanbourne@ymail.com>
2018-07-22 19:09:01 -0400

    added app to index.js for exporting to other repo

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-const express = require("express");
-const path = require("path");
-const jbt = require("jbt-express");
-
-const app = express();
-jbt.init({app});
-
-app.set("view engine", "jbt");
-app.set("views", "./views");
-
-app.use(express.static(path.join(__dirname, 'views')));
-app.use("/", require("./routes/routes"));
+const app = require("./index");
 
 app.listen(3000, () => {
     console.log("Starting server...");
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const app = express();
-
 const path = require("path");
+const jbt = require("jbt-express");
 
-app.use(express.static(path.join(__dirname, 'public')));
+const app = express();
+jbt.init({app});
+
+app.set("view engine", "jbt");
+app.set("views", "./views");
+
+app.use(express.static(path.join(__dirname, 'views')));
+app.use("/", require("./routes/routes"));
+
 
 module.exports = app;
